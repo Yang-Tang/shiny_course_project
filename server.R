90eed57cda5392cf4b319fc39ec31ce47650e64e
doubletang <doubletang@gmail.com>
2014-08-18 22:31:44 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,108 @@
+library(shiny)
+n = 6
+nums <- sample(rep(c(1:(n/2)), times=2), n, F)
+guessed <- show <- rep(F, times=n)
+lst <- as.list(c(1:n))
+names(lst) <- rep('X', times=n)
+num1 <- num2 <- -1
+idx <- 1
+
+shinyServer(function(input, output, session){
+    observe({
+        input$init
+        n <<- 2* as.numeric(input$dif)
+        nums <<- sample(rep(c(1:(n/2)), times=2), n, F)
+        guessed <<- show <<- rep(F, times=n)
+        lst <<- as.list(c(1:n))
+        names(lst) <<- rep('X', times=n)
+        isolate({updateCheckboxGroupInput(session, 'numbers', choices = lst, selected=NULL)})
+        output$msg <- renderText({''})
+        
+    })
+    
+    observe({
+        #input$dif
+        if(length(input$numbers) == 1){
+            idx <<- c(as.numeric(input$numbers[1]))
+            num1 <<- nums[idx[1]]
+            show[idx[1]] <<- T
+            names(lst)[idx[1]] <<- as.character(num1)
+            #                 isolate({
+            #                     updateCheckboxGroupInput(session, 'numbers', 
+            #                                             choices = lst, 
+            #                                             selected=as.character(idx[1])
+            #                     )
+            #                 })
+        } else if(length(input$numbers) == 2){
+            for(sel in input$numbers){
+                if (!(as.numeric(sel) %in% idx)) {
+                    idx <<- c(idx, as.numeric(sel))
+                }
+            }
+            num2 <<- nums[idx[2]]
+            show[idx[2]] <<- T
+            names(lst)[idx[2]] <<- as.character(num2)
+            if (num1 == num2) {
+                guessed[idx[1]] <<- guessed[idx[2]] <<- T
+            }
+            #                 isolate({updateCheckboxGroupInput(session, 'numbers', choices = lst)})
+        } else if(length(input$numbers) == 3){
+            for(sel in input$numbers){
+                if (!(as.numeric(sel) %in% idx)) {
+                    idx <<- c(as.numeric(sel))
+                    break
+                }
+            }
+            num1 <<- nums[idx[1]]
+            num2 <<- -1
+            show <<- guessed
+            show[idx[1]] <<- T
+            for(i in c(1:n)){
+                if(!show[i]){
+                    names(lst)[i] <<- 'X'
+                }
+            }
+            #                 isolate({updateCheckboxGroupInput(session, 'numbers', choices = lst, selected=as.character(idx[3]))})
+        } else if(length(input$numbers) == 0){
+#             if(!guessed[idx[1]]){
+#                 names(lst)[idx[1]] <<- 'X'
+#             }
+            for(i in c(1:n)){
+                if(!guessed[i]){
+                    names(lst)[i] <<- 'X'
+                }
+            }
+            idx <<- NULL
+            num1 <<- num2 <<- -1
+        }
+        isolate({
+            updateCheckboxGroupInput(session, 'numbers', 
+                                     choices = lst, 
+                                     selected=as.character(idx)
+            )
+        })
+        if(!(F %in% guessed)){
+            output$msg <- renderText({'Good job! Once more?'})
+        }
+        #             print(input$numbers)
+        #             print (idx)
+        #             print (show)
+        #             print (guessed)
+        #         if(length(input$numbers) >= 3){
+        #             isolate({updateCheckboxGroupInput(session, 'numbers', choices = lst, selected=NULL)})
+        #         } 
+        #             sapply(c(1:n), function(x){
+        #                 if(show[x]){
+        #                     return(as.character(nums[x]))
+        #                 } else {
+        #                     return('X')
+        #                 }
+        #             })
+    })
+    
+    #         output$msg <- renderText({
+    #             name()
+    #             #name()
+    #         })
+})
+
