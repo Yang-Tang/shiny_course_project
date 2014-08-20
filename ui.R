e1c67adab890fe313065159c9cff4e25b75f3cb4
doubletang <doubletang@gmail.com>
2014-08-20 08:32:58 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,11 +1,5 @@
 library(shiny)
-# n = 6
-# nums <- sample(rep(c(1:(n/2)), times=2), n, F)
-# guessed <- show <- rep(F, times=n)
-# lst <- as.list(c(1:n))
-# names(lst) <- rep('X', times=n)
-# num1 <- num2 <- -1
-# idx <- 1
+
 shinyUI(fluidPage(
     titlePanel('The Memory Match Game'),
     
@@ -25,7 +19,8 @@ shinyUI(fluidPage(
                                label = h3('Numbers'),
                                choices = ''
             ),
-            h3(textOutput('msg'))
+            h3(textOutput('msg')),
+            uiOutput('numbers')
         )
     )
 ))
